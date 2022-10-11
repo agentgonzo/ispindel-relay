9822601204e39cb10d9f9820f261cc00f2b93277
Steve Arch <sarch@cloudbees.com>
2022-10-11 16:01:58 +0100

    feat: Persist services config to file

--- server/services/destinations.ts
+++ server/services/destinations.ts
@@ -1,20 +1,29 @@
-import {IDestination, IHttpDestination, IInfluxDBDestination} from 'types'
+import {IDestination} from 'types'
+import * as fs from 'fs'
 
-let _destinations = [
-  {
-    type: 'HTTP',
-    url: 'http://httpbin.org/anything',
-    token: '',
-  } as IHttpDestination,
-  {
-    type: 'InfluxDB',
-    hostname: 'example.com',
-    port: '8443',
-    database: 'mydb',
-    error: "this is my error message",
-  } as IInfluxDBDestination
-] as IDestination[]
-// TODO. Load this from disk
+const configFileName = 'services.json'
+
+// let _destinations = [
+//   {
+//     type: 'HTTP',
+//     url: 'http://httpbin.org/anything',
+//     token: '',
+//   } as IHttpDestination,
+//   {
+//     type: 'InfluxDB',
+//     hostname: 'example.com',
+//     port: '8443',
+//     database: 'mydb',
+//     error: "this is my error message",
+//   } as IInfluxDBDestination
+// ] as IDestination[]
+
+let _destinations = [] as IDestination[]
+try {
+  _destinations = JSON.parse(fs.readFileSync(configFileName) as any)
+} catch {
+  console.warn(`Could not load services definiton from ${configFileName}`)
+}
 
 export const getDestinations = (): IDestination[] => {
   return _destinations
@@ -22,5 +31,5 @@ export const getDestinations = (): IDestination[] => {
 
 export const setDestinations = (destinations: IDestination[]) => {
   _destinations = destinations
-  // TODO Save these to disk
+  fs.writeFileSync(configFileName, JSON.stringify(destinations))
 }
