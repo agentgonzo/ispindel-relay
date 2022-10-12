32aab8b0378995bc4bc4d64031153da48a40270a
Steve Arch <sarch@cloudbees.com>
2022-10-12 15:50:27 +0100

    feat: store config file in user home dir
    This allows persisting the data in a docker volume

--- server/services/destinations.ts
+++ server/services/destinations.ts
@@ -1,7 +1,9 @@
 import {IDestination} from 'types'
 import * as fs from 'fs'
+import * as os from 'os'
+import path from 'path'
 
-const configFileName = 'services.json'
+const configFileName = path.join(os.homedir(), 'services.json')
 
 // let _destinations = [
 //   {
