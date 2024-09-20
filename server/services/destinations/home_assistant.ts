b2680d9a1a1f36b2acc0f1dd501d8bf1e5377a29
Steve Arch <steve.arch@personio.de>
2024-09-20 11:26:10 +0100

    Send other iSpindel data to homeassistant

--- server/services/destinations/home_assistant.ts
+++ server/services/destinations/home_assistant.ts
@@ -1,22 +1,52 @@
 import axios from 'axios'
 import {IHomeAssistantDestination, ISpindelData} from 'types'
 
+const sendSensorToHomeAssistant = async (
+    destination: IHomeAssistantDestination,
+    name: String,
+    state: Number,
+    attributes: Object,
+) => {
+    return axios.post(
+        `${destination.url}/api/states/sensor.ispindel_${name}`, // TODO make it configurable,
+        {
+            'state': state,
+            'attributes': attributes
+        },
+        {
+            headers: {
+                'Authorization': 'Bearer ' + destination.token,
+                'Content-Type': 'application/json',
+            }
+        }
+    )
+}
+
+const calculateBatteryPercentage = (voltage: number) => {
+    const high = 4.21
+    const low = 3.0
+    const range = high - low
+    return (voltage - low) / range * 100
+}
+
 export const sendToHomeAssistant = async (destination: IHomeAssistantDestination, data: ISpindelData) => {
-  const url = destination.url + '/api/states/sensor.' + 'ispindel_temperature' // TODO make it configurable
-  await axios.post(
-    url,
-    {
-      'state': data.temperature,
-      'attributes': {
-        'friendly_name': 'iSpindel Temperature',
-        'unit_of_measurement': '°C',
-      }
-    },
-    {
-      headers: {
-        'Authorization': 'Bearer ' + destination.token,
-        'Content-Type': 'application/json',
-      }
-    }
-  )
+    await Promise.all([
+            sendSensorToHomeAssistant(destination, 'temperature', data.temperature, {
+                'friendly_name': 'iSpindel Temperature',
+                'unit_of_measurement': '°C',
+                'device_class': 'temperature',
+            }),
+
+            sendSensorToHomeAssistant(destination, 'gravity', data.gravity, {'friendly_name': 'iSpindel Gravity'}),
+            sendSensorToHomeAssistant(destination, 'battery', calculateBatteryPercentage(data.battery), {
+                'friendly_name': 'iSpindel Battery',
+                'device_class': 'battery',
+            }),
+            sendSensorToHomeAssistant(destination, 'signal', data.RSSI, {
+                'friendly_name': 'iSpindel Signal Strength',
+                'device_class': 'signal_strength',
+            }),
+            sendSensorToHomeAssistant(destination, 'tilt', data.angle, {'friendly_name': 'iSpindel tilt angle'})
+        ]
+    )
 }
